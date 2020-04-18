72522f90152a7e409d425957aa48f6d4cba70233
Melanie Tran <melaniexx02@gmail.com>
2020-04-18 06:54:34 -0600

    Created test to make keys hash and changed attributes test

--- test/key_test.rb
+++ test/key_test.rb
@@ -11,7 +11,6 @@ class KeyTest < Minitest::Test
 
   def test_it_has_attributes
     assert_equal "01165", @key.number
-    assert_equal ({}), @key.keys
   end
 
   def test_can_generate_random_number
@@ -23,5 +22,12 @@ class KeyTest < Minitest::Test
 
   def test_can_split_string
     assert_equal ["0", "1", "1", "6", "5"], @key.split_string
+  end
+
+  def test_can_make_keys
+    expected = {
+      A: 01, B: 11, C: 16, D: 65
+    }
+    assert_equal expected, @key.make_keys
   end
 end
