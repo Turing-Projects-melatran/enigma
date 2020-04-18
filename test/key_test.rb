47ca26a0583639ca42a866d5837a291c1f7abc12
Melanie Tran <melaniexx02@gmail.com>
2020-04-17 21:53:37 -0600

    Created a test to generate random number

--- test/key_test.rb
+++ test/key_test.rb
@@ -11,5 +11,10 @@ class KeyTest < Minitest::Test
 
   def test_it_has_attributes
     assert_equal "01165", @key.number
+    assert_equal ({}), @key.keys
+  end
+
+  def test_can_generate_random_number
+    assert_equal "01307", @key.generate_random
   end
 end
