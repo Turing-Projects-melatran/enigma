cfce3b8337061cb6e9a0cda56e593c3de7e9ae46
Melanie Tran <melaniexx02@gmail.com>
2020-04-18 07:56:40 -0600

    Create test to make shift hash as well as another test when no arguments are given

--- test/shift_test.rb
+++ test/shift_test.rb
@@ -15,4 +15,21 @@ class ShiftTest < Minitest::Test
     assert_equal @keys, @shift.keys
     assert_equal @offset, @shift.offset
   end
+
+  def test_can_make_shift_hash
+    expected = {:A=>3, :B=>13, :C=>18, :D=>70}
+    assert_equal expected, @shift.make_shift
+  end
+
+  def test_can_make_shifts_with_no_arguments
+    keys = Key.new
+    offset = Offset.new
+    shift = Shift.new(keys, offset)
+
+    assert_instance_of Shift, shift
+    assert_equal keys, shift.keys
+    assert_equal offset, shift.offset
+
+    assert_equal 4, shift.make_shift.length
+  end
 end
