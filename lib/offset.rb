917013af455e11687176afd4ecd4391c5f9f8956
Melanie Tran <melaniexx02@gmail.com>
2020-04-17 22:37:59 -0600

    Created test for offset hash and method to crate offset hash

--- lib/offset.rb
+++ lib/offset.rb
@@ -10,4 +10,18 @@ class Offset
   def square
     (@date.to_i * @date.to_i).to_s
   end
+
+  def last_four
+    square.split('')[-4..-1]
+  end
+
+  def make_offset
+    offset = {
+      A: last_four[0].to_i,
+      B: last_four[1].to_i,
+      C: last_four[2].to_i,
+      D: last_four[3].to_i
+    }
+    offset
+  end
 end
--- test/offset_test.rb
+++ test/offset_test.rb
@@ -2,15 +2,15 @@ require './test/test_helper'
 
 class OffsetTest < Minitest::Test
   def setup
-    @offset = Offset.new("092415")
+    @date = Offset.new("092415")
   end
 
   def test_it_exists
-    assert_instance_of Offset, @offset
+    assert_instance_of Offset, @date
   end
 
   def test_it_has_attributes
-    assert_equal "092415", @offset.date
+    assert_equal "092415", @date.date
   end
 
   def test_can_test_todays_date
@@ -20,6 +20,20 @@ class OffsetTest < Minitest::Test
   end
 
   def test_can_square_the_date
-    assert_equal "8540532225", @offset.square
+    assert_equal "8540532225", @date.square
+  end
+
+  def test_can_take_last_four_digits
+    @date.square
+
+    assert_equal ["2", "2", "2", "5"], @date.last_four
+  end
+
+  def test_can_create_date_hash
+    @date.square
+    @date.last_four
+    expected = {A: 2, B: 2, C: 2, D: 5}
+
+    assert_equal expected, @date.make_offset
   end
 end
