5af94be6078197b8137560d254d4f6ccd0e61e5d
Melanie Tran <melaniexx02@gmail.com>
2020-04-18 07:56:54 -0600

    Add method to make shift hashes

--- lib/shift.rb
+++ lib/shift.rb
@@ -8,4 +8,10 @@ class Shift
     @keys = keys
     @offset = offset
   end
+
+  def make_shift
+    keys_hash = @keys.make_keys
+    offset_hash = @offset.make_offset
+    keys_hash.merge(offset_hash){|key, oldval, newval| newval + oldval}
+  end
 end
