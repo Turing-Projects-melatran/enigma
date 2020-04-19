2228a0282766cde3c802f21dd250417be0c2776f
Melanie Tran <melaniexx02@gmail.com>
2020-04-19 00:11:05 -0600

    Created test and methods for encryption and decryption

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -3,13 +3,27 @@ require './lib/offset'
 require './lib/shift'
 
 class Enigma
+  def encrypt(message, key = nil, date = nil)
+    keys = Key.new(key)
+    offset = Offset.new(date)
+    shift = Shift.new(keys, offset)
+    encrypt_hash = {}
+    encrypt_hash[:encryption] = shift.encryption(message)
+    encrypt_hash[:key] = shift.key.number.to_s
+    encrypt_hash[:date] = shift.offset.date.to_s
+    encrypt_hash
+  end
 
-  # def encrypt(message, key, offset)
-  #   encrypt_hash = {}
-  #   encrypt_hash[:encryption]
-  #   encrypt_hash[:key]
-  #   encrypt_hash[:offset]
-  # end
+  def decrypt(ciphertext, key = nil, date = nil)
+    keys = Key.new(key)
+    offset = Offset.new(date)
+    shift = Shift.new(keys, offset)
+    decrypt_hash = {}
+    decrypt_hash[:decryption] = shift.decryption(ciphertext)
+    decrypt_hash[:key] = shift.key.number.to_s
+    decrypt_hash[:date] = shift.offset.date.to_s
+    decrypt_hash
+  end
 end
 
 #encrypt shift forwards
--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -4,8 +4,26 @@ class EnigmaTest < Minitest::Test
   def setup
     @enigma = Enigma.new
   end
-  
+
   def test_it_exists
     assert_instance_of Enigma, @enigma
+  end
+
+  def test_it_can_encrypt
+    expected = {
+      encryption: "keder ohulw",
+      key: "02715",
+      date: "040895"
+      }
+      assert_equal expected, @enigma.encrypt("hello world", "02715", "040895")
+  end
+
+  def test_it_can_decrypt
+    expected =  {
+      decryption: "hello world",
+      key: "02715",
+      date: "040895"
+      }
+      assert_equal expected, @enigma.decrypt("keder ohulw", "02715", "040895")
   end
 end
