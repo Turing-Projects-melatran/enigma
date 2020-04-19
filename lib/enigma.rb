1fb2be9417b0abaca79d1a18cb46b403398ed2af
Melanie Tran <melaniexx02@gmail.com>
2020-04-18 21:48:33 -0600

    Create engima class and test it exists

---
+++ lib/enigma.rb
@@ -0,0 +1,16 @@
+require './lib/key'
+require './lib/offset'
+require './lib/shift'
+
+class Enigma
+
+  # def encrypt(message, key, offset)
+  #   encrypt_hash = {}
+  #   encrypt_hash[:encryption]
+  #   encrypt_hash[:key]
+  #   encrypt_hash[:offset]
+  # end
+end
+
+#encrypt shift forwards
+#decrypt shift backwards
