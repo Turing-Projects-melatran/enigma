4e5fc8a59ac029f9bd714e4aa8bc598267687548
Melanie Tran <melaniexx02@gmail.com>
2020-04-20 13:37:49 -0600

    Changed number to keys to make it easier to read

--- lib/key.rb
+++ lib/key.rb
@@ -1,16 +1,16 @@
 class Key
-  attr_reader :number
+  attr_reader :key
 
-  def initialize(number = rand(0..9999).to_s.rjust(5, "0"))
-    @number = number
+  def initialize(key = rand(0..9999).to_s.rjust(5, "0"))
+    @key = key
   end
 
   def make_keys
     {
-      A: number[0..1].to_i,
-      B: number[1..2].to_i,
-      C: number[2..3].to_i,
-      D: number[3..4].to_i
+      A: key[0..1].to_i,
+      B: key[1..2].to_i,
+      C: key[2..3].to_i,
+      D: key[3..4].to_i
     }
   end
 end
--- test/key_test.rb
+++ test/key_test.rb
@@ -10,7 +10,7 @@ class KeyTest < Minitest::Test
   end
 
   def test_it_has_attributes
-    assert_equal "01165", @key.number
+    assert_equal "01165", @key.key
   end
 
   def test_can_make_keys
@@ -20,15 +20,15 @@ class KeyTest < Minitest::Test
     assert_equal expected, @key.make_keys
   end
 
-  def test_random_number
+  def test_random_key
     new = Key.new
-    new.stubs(:number).returns("02343")
-    assert_equal "02343", new.number
+    new.stubs(:key).returns("02343")
+    assert_equal "02343", new.key
   end
 
-  def test_random_number_to_make_keys
+  def test_random_key_to_make_keys
     new = Key.new
-    new.stubs(:number).returns("02343")
+    new.stubs(:key).returns("02343")
 
     expected = {A: 02, B: 23, C: 34, D: 43}
     assert_equal expected, new.make_keys
