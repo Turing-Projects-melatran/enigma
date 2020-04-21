067be9b427b24e8d39be757224a6bd1b0a6b0f23
Melanie Tran <melaniexx02@gmail.com>
2020-04-20 18:58:09 -0600

    Created encrypt and decrypt runner files that work

--- lib/decrypt.rb
+++ lib/decrypt.rb
@@ -1,1 +1,15 @@
 require './lib/enigma'
+
+enigma = Enigma.new
+
+handle = File.open(ARGV[0], "r")
+text = handle.read
+
+decrypted_message = enigma.decrypt(text.chomp, ARGV[2], ARGV[3])
+
+writer = File.open(ARGV[1], "w")
+writer.write(decrypted_message[:decryption])
+handle.close
+writer.close
+
+p "Created #{ARGV[1]} with the key #{decrypted_message[:key]} and a date #{decrypted_message[:date]}"
